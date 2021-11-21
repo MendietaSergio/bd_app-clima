6f4b623f66c09eef668dc701d7dc02244fd78a68
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-20 21:09:36 -0300

    añade ruta delete, añade log para saber en que puerto esta

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,7 @@ const listWeatherController = require('../controller/indexController')
 router.get('/', listWeatherController.home);
 router.get('/listWeather', listWeatherController.list);
 router.post('/listWeather', listWeatherController.add);
+router.get('/detailWeather',listWeatherController.detail)
+router.delete('/delete/:id', listWeatherController.delete)
 
 module.exports = router;
